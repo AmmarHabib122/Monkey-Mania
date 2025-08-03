3036830c25a2ebd04cdb7394614af7134e016ab7
Muhammad Tareq <mt.abulnasr@gmail.com>
2025-08-03 18:35:53 +0300

    Allow setting the host from the env file

--- MonkeyMania/settings.py
+++ MonkeyMania/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']  # Change if you bind a custom domain
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=lambda v: [s.strip() for s in v.split(",")])  # Change if you bind a custom domain
 
 INSTALLED_APPS = [
     'django.contrib.admin',
