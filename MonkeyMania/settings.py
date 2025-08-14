ac34f2c64c495a850c69784709ae144016de3183
AmmarHabib122 <ammar@63780@gmail.com>
2025-08-14 17:53:50 +0300

    add pagination to list all children

--- MonkeyMania/settings.py
+++ MonkeyMania/settings.py
@@ -138,6 +138,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
     'EXCEPTION_HANDLER': 'base.utils.custom_exception_handler.custom_exception_handler',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10  # Number of items per page
     
 }
 
