0a670003b8372d043c462cbf566c14d1b06e13ae
Ammar Hamed <ammarhamed633@gmail.com>
2025-08-28 16:11:24 +0300

    fix: update string representation of Product and User models

--- base/models/UserModels.py
+++ base/models/UserModels.py
@@ -50,7 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     REQUIRED_FIELDS = ['username']
     
     def __str__(self):
-        return f"#{self.id} {self.name}"
+        return f"#{self.id} {self.username}"
     
     
 
