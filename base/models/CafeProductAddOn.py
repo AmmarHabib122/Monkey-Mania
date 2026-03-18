dfc67a7b27edb75d63f612d8198e25ed756f5eaa
AmmarHabib27 <a.hammed@yanabea-app.com>
2026-03-18 17:33:26 +0200

    fix naming issue with models

--- base/models/CafeProductAddOn.py
+++ base/models/CafeProductAddOn.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 
-class CafeProductAddOn(models.Model):
+class CafeProductAddOns(models.Model):
     name                 = models.CharField(max_length = 150, unique = True)
     image                = models.CharField(max_length = 255)
     material             = models.ForeignKey('base.Material', on_delete = models.PROTECT, related_name = 'cafe_product_toppings_set')
